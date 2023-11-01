a3a5b3a62bcffd2756b8256ba7d2442c969db551
Igor Morozov <igmorv@gmail.com>
2023-10-31 23:12:26 -0700

    some formatting

--- pkg/verify/verify.h
+++ pkg/verify/verify.h
@@ -1,13 +1,8 @@
 #ifndef __VERIFY_H__
 #define __VERIFY_H__
 
-int verify(void* cert_buff,
-           int cert_len,
-           void* roots,
-           int roots_len,
-           int roots_lens[],
-           void* intermediates,
-           int intermediates_len,
-           int intermediates_lens[]);
+int verify(void* cert_buff, int cert_len, // leaf certificate
+           void* roots, int roots_len, int roots_lens[], // root certificates, trusted chain
+           void* intermediates, int intermediates_len, int intermediates_lens[]); // intermediate certificates, untrusted chain
 
 #endif //__VERIFY_H__
